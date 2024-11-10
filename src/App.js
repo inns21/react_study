fe9b5bf55d83c535ab47f3e25039c64d862502f8
inns21 <inns21@gmail.com>
2024-11-10 20:44:09 +0900

    클래스형을 함수형으로 변경하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,128 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Myheader from './component/Myheader';
 import Mynav from './component/Mynav';
 import ReadArticle from './component/ReadArticle';
 import CreateArticle from './component/CreateArticle';
 import UpdateArticle from './component/UpdateArticle';
 
+const App = () => {
+  // 변경이 많이 될거다? state
+  // 변경이 많이 안된다? 일반 변수
+  let max_menu_id = 3;
+  let welcome = {
+    title: 'Hello',
+    desc: 'Welcome to FrontEnd'
+  }
+  let subject = {
+    title: '프론트엔드 개발자 역량',
+    desc: '기본언어인 HTML, CSS, JAVASCRIPT부터 학습합니다'
+  }
+  const [mode, setMode] = useState('welcome');
+  const [selected_id, setSelectedId] = useState(2);
+  const [menus, setMenus] = useState([
+    {id:1, title:'UI/UX 개발 ', desc:'사용자 인터페이스와 사용자가 웹사이트를 이용하면 느끼고 생각하는 총체적 경험을 개발 '},
+    {id:2, title:'재사용이 가능한 UI 개발 ', desc:'앵귤러, 리엑트, 뷰등의 자바스크립트 프레임워크를 가지고 재사용할 수 있는 UI를 만든다.  '},
+    {id:3, title:'애니메이션 구현  ', desc:'CSS 또는 javascript를 사용해 다양한 효과의 애니메이션 구현한다.'},
+  ]);
+
+  const getReadArticle = () => {
+    let idx = menus.findIndex((item) => (item.id === selected_id));
+    let data = menus[idx];
+    return data;
+  }
+
+  const getArticles = () => {
+    let _title, _desc, _article = null;
+    if (mode === 'welcome') {
+      _title = welcome.title;
+      _desc = welcome.desc;
+      _article = <ReadArticle title={_title} desc={_desc} mode={mode}></ReadArticle>
+    } else if (mode === 'read') {
+
+      let _data = getReadArticle()
+
+      _article = <ReadArticle title={_data.title} desc={_data.desc} onChangeMode={(_mode) => {
+        if (_mode === 'delete') {
+          if (window.confirm("정말 삭제할까요?")) {
+            let _menus = Array.from(menus);
+            let idx = _menus.findIndex(item => (item.id === selected_id));
+            console.log(idx)
+            _menus.splice(idx, 1);
+
+            setMenus(_menus);
+            setMode('welcome');
+
+          }
+
+        } else {
+          setMode('update');
+        }
+      }}></ReadArticle>
+
+    } else if (mode === 'create') {
+      _article = <CreateArticle onSubmit={(_title, _desc) => {
+        max_menu_id += 1;
+
+        let _menus = Array.from(menus);
+        _menus.push({id:max_menu_id, title:_title, desc:_desc})
+
+        setMenus(_menus);
+        setMode('read');
+        setSelectedId(max_menu_id);
+
+      }}></CreateArticle>
+    } else if (mode === 'update') {
+      let _content = getReadArticle();
+      _article = <UpdateArticle data={ _content} onSubmit={(_id,_title, _desc) => {
+
+        let _menus = Array.from(menus);
+        _menus.forEach((item, index) => {
+          if (item.id === _id) {
+            _menus[index] = {id:_id, title:_title, desc:_desc}
+          }
+        });
+
+        setMenus(_menus);
+        setMode('read');
+
+      }}></UpdateArticle>
+
+    }
+    return _article;
+  }
+  return (
+    <div className='App'>
+      <Myheader
+          title={subject.title}
+          desc={subject.desc}
+          onChangeMode = {() => {
+            setMode('welcome');
+          }}>
+      </Myheader>
+      <Mynav data={menus} onChangePage={(id) => {
+        setMode('read');
+        setSelectedId(id);
+      }}></Mynav>
+
+      {getArticles()}
+
+      <hr/>
+        <div className="menu">
+          <button type='button' className='primary' onClick={() => {
+          setMode('create');
+          }}>
+            Create task
+          </button>
+        </div>
+    </div>
+  )
+}
+
+export default App;
+
+
+/*
 class App extends Component {
   constructor(props) {
     super(props);
@@ -30,25 +147,25 @@ class App extends Component {
   }
 
   getReadArticle() {
-    let idx = this.state.menus.findIndex((item) => (item.id === this.state.selected_id));
-    let data = this.state.menus[idx];
+    let idx = menus.findIndex((item) => (item.id === selected_id));
+    let data = menus[idx];
     return data;
   }
 
   // welcome, read, create 함수로 만들기
   getArticles() {
     let _title, _desc, _article = null;
-    if (this.state.mode === 'welcome') {
-      _title = this.state.welcome.title;
-      _desc = this.state.welcome.desc;
-      _article = <ReadArticle title={_title} desc={_desc} mode={this.state.mode}></ReadArticle>
-    } else if (this.state.mode === 'read') {
+    if (mode === 'welcome') {
+      _title = welcome.title;
+      _desc = welcome.desc;
+      _article = <ReadArticle title={_title} desc={_desc} mode={mode}></ReadArticle>
+    } else if (mode === 'read') {
       let _data = this.getReadArticle()
       _article = <ReadArticle title={_data.title} desc={_data.desc} onChangeMode={(_mode) => {
         if (_mode === 'delete') {
           if (window.confirm("정말 삭제할까요?")) {
-            let _menus = Array.from(this.state.menus);
-            let idx = _menus.findIndex(item => (item.id === this.state.selected_id));
+            let _menus = Array.from(menus);
+            let idx = _menus.findIndex(item => (item.id === selected_id));
             console.log(idx)
             _menus.splice(idx, 1);
             this.setState({
@@ -65,24 +182,24 @@ class App extends Component {
         }
       }}></ReadArticle>
 
-    } else if (this.state.mode === 'create') {
+    } else if (mode === 'create') {
       _article = <CreateArticle onSubmit={(_title, _desc) => {
         console.log(_title, _desc);
         this.max_menu_id += 1;
-        // let _menus = this.state.menus.concat({id: this.max_menu_id, title: _title, desc: _desc})
+        // let _menus = menus.concat({id: this.max_menu_id, title: _title, desc: _desc})
         // this.setState({
         //   menus: _menus
         // });
 
         // 생명주기 함수 shouldComponentUpdate에 적용이 안 됨
-        // this.state.menus.push(
+        // menus.push(
         //   {id:this.max_menu_id, title:_title, desc:_desc}
         // )
         // this.setState({
-        //   menus:this.state.menus
+        //   menus:menus
         // });
 
-        let _menus = Array.from(this.state.menus);
+        let _menus = Array.from(menus);
         _menus.push({id:this.max_menu_id, title:_title, desc:_desc})
         this.setState({
           menus: _menus,
@@ -91,11 +208,11 @@ class App extends Component {
         });
 
       }}></CreateArticle>
-    } else if (this.state.mode === 'update') {
+    } else if (mode === 'update') {
       let _content = this.getReadArticle();
       _article = <UpdateArticle data={ _content} onSubmit={(_id,_title, _desc) => {
 
-        let _menus = Array.from(this.state.menus);
+        let _menus = Array.from(menus);
         _menus.forEach((item, index) => {
           if (item.id === _id) {
             _menus[index] = {id:_id, title:_title, desc:_desc}
@@ -117,15 +234,15 @@ class App extends Component {
     return (
       <div className='App'>
         <Myheader
-          title={this.state.subject.title}
-          desc={this.state.subject.desc}
+          title={subject.title}
+          desc={subject.desc}
           onChangeMode = {() => {
             this.setState({
               mode: 'welcome'
             })
           }}>
         </Myheader>
-        <Mynav data={this.state.menus} onChangePage={(id) => {
+        <Mynav data={menus} onChangePage={(id) => {
           this.setState({
             mode: 'read',
             selected_id: id
@@ -144,11 +261,11 @@ class App extends Component {
           }}>
             Create task
           </button>
-          {/* <a href="/create" class="primary">Create task</a> */}
         </div>
       </div>
     )
   }
 }
+ */
 
-export default App;
+
--- src/component/Mynav.js
+++ src/component/Mynav.js
@@ -1,5 +1,40 @@
-import React, { Component } from "react"
+import React, { Component, useEffect, useState } from "react"
 
+const Mynav = ({ data, onChangePage }) => {
+  // 부모로부터 받은 값을 변경되었는지 확인? state에 넣어야함
+  const [list, setList] = useState([]);
+  let lists = [];
+  const getList = () => {
+    data.forEach(item => {
+      lists.push(
+        <li key={item.id}>
+          <a href="/" onClick={(e) => {
+            e.preventDefault();
+            onChangePage(item.id);
+          }}>
+            {item.title}
+          </a>
+        </li>
+      );
+    });
+    setList(lists);
+  }
+  useEffect(() => {
+    getList();
+    console.log('getList 실행');
+  }, [data]);
+  return (
+    <nav>
+      <ul>
+        {list}
+      </ul>
+    </nav>
+  )
+}
+
+export default Mynav;
+
+/*
 class Mynav extends Component{
 
   //  렌더 할 지 말 지 결정
@@ -41,5 +76,4 @@ class Mynav extends Component{
     )
   }
 }
-
-export default Mynav;
+*/
