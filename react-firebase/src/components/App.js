06b9b469746cecabae3d235f9b5e8b9e3eb407cd
inns21 <inns21@gmail.com>
2024-11-18 21:15:20 +0900

    회원가입

--- react-firebase/src/components/App.js
+++ react-firebase/src/components/App.js
@@ -1,11 +1,29 @@
-import Auth from '../routes/Auth';
+import React, { useState } from "react";
+import AppRouter from './Router';
+import { authService } from "../firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+console.log(authService);
 
 function App() {
-  return (
-    <div>
-      <Auth></Auth>
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setIsLoggedIn(true)
+    } else {
+      setIsLoggedIn(false);
+    }
+    setInit(true);
+  });
 
-    </div>
+  return (
+    <>
+      {
+        init ? <AppRouter isLoggedIn={isLoggedIn}></AppRouter> : "회원 정보 확인 중..."
+      }
+    </>
   );
 }
 
--- react-firebase/src/routes/Auth.js
+++ react-firebase/src/routes/Auth.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
+
+  const auth = getAuth();
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password'){
+      setPassword(value);
+    }
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (newAccount) {
+      // 회원가입
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        setError(errorMessage);
+      });
+    } else {
+      // 로그인
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        setError(errorMessage);
+      });
+    }
+  }
+  const toggleAccount = () => setNewAccount(prev => !prev);
   return (
-    <div>Auth</div>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name='email' type='email' placeholder='Email' required value={email} onChange={onChange}></input>
+        <input name='password' type='password' placeholder='password' required value={password} onChange={onChange}></input>
+        <button>{newAccount ? "Create Account" : "Login"}</button>
+        {error}
+      </form>
+      <div onClick={toggleAccount}>{newAccount ? "Login" : "Create Account"}</div>
+    </div>
   )
 }
 export default Auth;
